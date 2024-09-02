0f124624f40c106e7f8adfc877c2ecdac05f7296
Feyz Ibrahim <46452412+feyzibrahim@users.noreply.github.com>
2024-09-02 15:33:17 +0530

    feat: added the landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,221 @@
-import Image from "next/image";
+"use client";
+import Link from "next/link";
+import { useEffect, useRef, useState } from "react";
+
+const SNAKE_SIZE = 10;
+const FOOD_SIZE = 10;
+const CANVAS_SIZE = 400;
+const INIT_SIZE = 5; // Initial snake size
+const INIT_POSITION = Array.from({ length: INIT_SIZE }, (_, i) => ({
+	x: CANVAS_SIZE / 4 - i * SNAKE_SIZE,
+	y: CANVAS_SIZE / 4,
+}));
+const SPEED = 10; // Adjust this for smoother/faster movement
+
+const getRandomPosition = () => ({
+	x: Math.floor((Math.random() * CANVAS_SIZE) / SNAKE_SIZE) * SNAKE_SIZE,
+	y: Math.floor((Math.random() * CANVAS_SIZE) / SNAKE_SIZE) * SNAKE_SIZE,
+});
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+	const canvasRef = useRef<HTMLCanvasElement | null>(null);
+	const [snake, setSnake] = useState(INIT_POSITION);
+	const [food, setFood] = useState(getRandomPosition());
+	const [direction, setDirection] = useState({ x: 1, y: 0 });
+	const [isGameOver, setIsGameOver] = useState(false);
+	const [isGameStarted, setIsGameStarted] = useState(false);
+	const [points, setPoints] = useState(0);
+	const [highScore, setHighScore] = useState(0);
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+	const handleKeyPress = (e: KeyboardEvent) => {
+		switch (e.key) {
+			case "ArrowUp":
+				if (direction.y !== 1) setDirection({ x: 0, y: -1 });
+				break;
+			case "ArrowDown":
+				if (direction.y !== -1) setDirection({ x: 0, y: 1 });
+				break;
+			case "ArrowLeft":
+				if (direction.x !== 1) setDirection({ x: -1, y: 0 });
+				break;
+			case "ArrowRight":
+				if (direction.x !== -1) setDirection({ x: 1, y: 0 });
+				break;
+		}
+	};
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+	const moveSnake = () => {
+		const newSnake = [...snake];
+		const head = {
+			x: snake[0].x + direction.x * SNAKE_SIZE,
+			y: snake[0].y + direction.y * SNAKE_SIZE,
+		};
+		newSnake.unshift(head);
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+		// Check for collision with food
+		if (head.x === food.x && head.y === food.y) {
+			setFood(getRandomPosition());
+			setPoints(points + 1);
+			if (points + 1 > highScore) {
+				setHighScore(points + 1);
+			}
+		} else {
+			newSnake.pop();
+		}
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+		// Check for collision with walls or itself
+		if (
+			head.x < 0 ||
+			head.y < 0 ||
+			head.x >= CANVAS_SIZE ||
+			head.y >= CANVAS_SIZE ||
+			newSnake
+				.slice(1)
+				.some((segment) => segment.x === head.x && segment.y === head.y)
+		) {
+			setIsGameOver(true);
+			setIsGameStarted(false);
+		} else {
+			setSnake(newSnake);
+		}
+	};
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+	const startGame = () => {
+		setSnake(INIT_POSITION);
+		setFood(getRandomPosition());
+		setDirection({ x: 1, y: 0 });
+		setPoints(0);
+		setIsGameOver(false);
+		setIsGameStarted(true);
+	};
+
+	const restartGame = () => {
+		startGame();
+	};
+
+	useEffect(() => {
+		let intervalId: NodeJS.Timeout;
+		if (isGameStarted && !isGameOver) {
+			intervalId = setInterval(moveSnake, 1000 / SPEED);
+		}
+		return () => clearInterval(intervalId);
+
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [snake, direction, isGameStarted, isGameOver]);
+
+	useEffect(() => {
+		window.addEventListener("keydown", handleKeyPress);
+		return () => window.removeEventListener("keydown", handleKeyPress);
+
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [direction]);
+
+	useEffect(() => {
+		const canvas = canvasRef.current;
+		if (canvas) {
+			const ctx = canvas.getContext("2d");
+			if (ctx) {
+				ctx.clearRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
+
+				// Draw the snake
+				snake.forEach((segment, index) => {
+					const opacity = 1 - index / snake.length;
+					ctx.fillStyle = `rgba(121, 166, 23, ${opacity})`;
+					ctx.fillRect(segment.x, segment.y, SNAKE_SIZE, SNAKE_SIZE);
+				});
+
+				// Draw the food as a stack of three circles with different sizes
+				const baseRadius = FOOD_SIZE / 2;
+				const centerX = food.x + baseRadius;
+				const centerY = food.y + baseRadius;
+
+				const circles = [
+					{ radius: baseRadius, opacity: 1 },
+					{ radius: baseRadius * 1.6, opacity: 0.5 },
+					{ radius: baseRadius * 2.2, opacity: 0.25 },
+				];
+
+				circles.forEach((circle, index) => {
+					ctx.beginPath();
+					ctx.arc(centerX, centerY, circle.radius, 0, 2 * Math.PI);
+					ctx.fillStyle = `rgba(121, 166, 23, ${circle.opacity})`;
+					ctx.fill();
+				});
+			}
+		}
+	}, [snake, food]);
+
+	return (
+		<div className="grid grid-cols-1 gap-5 lg:gap-0 lg:grid-cols-2 bg-background h-screen">
+			<div className="w-full absolute h-10 bg-primary flex items-center justify-center text-white">
+				Site is under construction
+			</div>
+			<div className="px-5 lg:pl-20 pt-20 lg:pt-40 space-y-5">
+				<p className="text-primary">Hello there, I&apos;m</p>
+				<h1 className="text-3xl lg:text-7xl font-bold mb-4 text-foreground">
+					Feyz Ibrahim.
+				</h1>
+				<h1 className="text-3xl lg:text-6xl font-bold mb-4 text-foreground-secondary">
+					Software Engineer
+				</h1>
+				<h3>
+					I&apos;m a software engineer specializing in building (and designing)
+					exceptional web, server and mobile apps. Currently, I&apos;m focused
+					on building advanced, medical-centered products at{" "}
+					<Link
+						className="text-primary hover:underline"
+						href="https://www.basys.ai/"
+						target="_blank"
+					>
+						basys.ai
+					</Link>
+					.
+				</h3>
+			</div>
+			<div className="h-full lg:flex items-center justify-center hidden">
+				<div>
+					<p className="text-center mb-1">
+						While you wait, enjoy this snake game!
+					</p>
+					<p className="text-xs mb-2 text-center text-foreground-secondary">
+						(Use arrows)
+					</p>
+
+					<div className="bg-background-secondary rounded-lg relative w-fit">
+						<canvas
+							ref={canvasRef}
+							width={CANVAS_SIZE}
+							height={CANVAS_SIZE}
+						/>
+						<div className="absolute inset-0 flex flex-col items-center justify-center">
+							{!isGameStarted && !isGameOver && (
+								<button
+									onClick={startGame}
+									className="bg-background text-foreground px-4 py-2 rounded mb-4"
+								>
+									Start Game
+								</button>
+							)}
+							{isGameOver && (
+								<>
+									<h2 className="text-red-500 mb-4">Game Over!</h2>
+									<button
+										onClick={restartGame}
+										className="bg-background text-foreground px-4 py-2 rounded"
+									>
+										Restart Game
+									</button>
+								</>
+							)}
+						</div>
+					</div>
+
+					<div className="flex justify-between w-64 mt-4">
+						<div>Points: {points}</div>
+						<div>High Score: {highScore}</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
